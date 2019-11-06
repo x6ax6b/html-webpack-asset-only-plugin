0690787076cec8cb689d971cc9b7428799c0bfed
Jun-Kyu Kim <x6ax6b@gmail.com>
2019-11-06 15:43:08 +0900

    0.2.0

--- plugin.js
+++ plugin.js
@@ -1,30 +1,63 @@
+const fs = require('fs');
+const path = require('path');
+const mkdirp = require('mkdirp');
+
 class HtmlWebpackAssetOnlyPlugin {
   pluginName = 'HtmlWebpackAssetOnlyPlugin';
+
+  constructor(options) {
+    if (options && options.assetOnly) {
+      this.assetOnly = true;
+    }
+  }
 
   apply(compiler) {
     compiler.hooks.compilation.tap(this.pluginName, (compilation) => {
       compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing.tapAsync(this.pluginName, (htmlPluginData, callback) => {
-        if (htmlPluginData.plugin.options.inject === 'head' || htmlPluginData.plugin.options.inject === 'body') {
+        if (this.assetOnly && htmlPluginData.plugin.options.inject !== false) {
           htmlPluginData.html = '';
         }
+
         callback(null, htmlPluginData);
       });
+
       compilation.hooks.htmlWebpackPluginAfterHtmlProcessing.tapAsync(this.pluginName, (htmlPluginData, callback) => {
-        let re;
-        if (htmlPluginData.plugin.options.inject === 'head') {
-          re = /(<link.*?)<script/;
-        } else if (htmlPluginData.plugin.options.inject === 'body') {
-          re = /(<script.*<\/script>)$/;
+        if (this.assetOnly && htmlPluginData.plugin.options.inject !== false) {
+          this.html = htmlPluginData.html;
+
+          const fullPath = path.resolve(compilation.compiler.outputPath, htmlPluginData.outputName);
+
+          mkdirp(path.dirname(fullPath), (mkdirpError) => {
+            if (mkdirpError) {
+              return callback(mkdirpError);
+            }
+
+            fs.writeFile(fullPath.replace(/(\.[^.]*$|$)/, '-script$1'), this.getScriptAsset(), (writeFileError) => {
+              if (writeFileError) {
+                return callback(writeFileError);
+              }
+
+              htmlPluginData.html = this.getLinkAsset();
+
+              callback(null, htmlPluginData);
+            });
+          });
+        } else {
+          callback(null, htmlPluginData);
         }
-        if (re) {
-          const asset = htmlPluginData.html.match(re);
-          if (asset) {
-            [, htmlPluginData.html] = asset;
-          }
-        }
-        callback(null, htmlPluginData);
       });
     });
   }
+
+  getLinkAsset() {
+    const matched = this.html.match(/(<l.*?)<[/s]/);
+    return matched ? matched[1] : '';
+  }
+
+  getScriptAsset() {
+    const matched = this.html.match(/(<s.*t>)/);
+    return matched ? matched[1] : '';
+  }
 }
+
 module.exports = HtmlWebpackAssetOnlyPlugin;
